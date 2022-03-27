e2cb7e656f80c0fd3ea18ae82a0e6d1b93cd4f0e
aguscontreras <35529189+aguscontreras@users.noreply.github.com>
2022-03-26 21:57:51 -0300

    imports

--- src/app/modules/menu/menu.module.ts
+++ src/app/modules/menu/menu.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuRoutingModule } from './menu-routing.module';
+import { MaterialModule } from '../material/material.module';
+import { SharedModule } from '../shared/shared.module';
+
+import { MenuService } from './services/menu.service';
+
 import { MainMenuComponent } from './pages/main-menu/main-menu.component';
 import { MenuItemComponent } from './components/menu-item/menu-item.component';
-import { MaterialModule } from '../material/material.module';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
+  imports: [CommonModule, MenuRoutingModule, MaterialModule, SharedModule],
   declarations: [MainMenuComponent, MenuItemComponent],
-  imports: [CommonModule, MenuRoutingModule, MaterialModule, HttpClientModule],
+  providers: [MenuService],
 })
 export class MenuModule {}
