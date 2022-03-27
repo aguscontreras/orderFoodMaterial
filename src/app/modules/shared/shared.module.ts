9deade3122574a98d049bf1d414b3008ccc0e811
aguscontreras <35529189+aguscontreras@users.noreply.github.com>
2022-03-26 21:58:28 -0300

    importaciones modules

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material/material.module';
 import { MainSidebarComponent } from './components/main-sidebar/main-sidebar.component';
+import { FilterChipsComponent } from './components/filter-chips/filter-chips.component';
 
-const sharedComponents = [MainSidebarComponent];
+const sharedComponents = [MainSidebarComponent, FilterChipsComponent];
 
 @NgModule({
   declarations: [...sharedComponents],
-  imports: [MaterialModule],
+  imports: [CommonModule, MaterialModule],
   exports: [...sharedComponents],
 })
 export class SharedModule {}
