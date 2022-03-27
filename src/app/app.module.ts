94b74e23967198b00eee5968521917f6a2e78fd8
aguscontreras <35529189+aguscontreras@users.noreply.github.com>
2022-03-26 21:57:13 -0300

    import menu module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from './modules/shared/shared.module';
 import { MaterialModule } from './modules/material/material.module';
+import { SharedModule } from './modules/shared/shared.module';
+import { MenuModule } from './modules/menu/menu.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
     MaterialModule,
+    MenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
